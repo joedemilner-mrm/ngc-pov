2766aaa42d8b31cc5a25da164d43cf33849b981a
Joe DeMilner <joe.demilner>
2023-05-02 13:23:01 -0400

    test

--- blocks/joesample/joesample.js
+++ blocks/joesample/joesample.js
@@ -1,3 +1,3 @@
 fetch('https://test-changes--ngc-pov--joedemilner-mrm.hlx.page/data.json')
   .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
-  .then(data => console.log(data));
+  .then(data => document.write(data));
