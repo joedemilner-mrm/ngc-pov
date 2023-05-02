b8ff9a913a5e456c77cb25a8f5c5fad628c4409e
Joe DeMilner <joe.demilner>
2023-05-02 13:27:08 -0400

    test

--- blocks/joesample/joesample.js
+++ blocks/joesample/joesample.js
@@ -1,3 +1,22 @@
-fetch('https://test-changes--ngc-pov--joedemilner-mrm.hlx.page/data.json')
+export default function decorate(block) {
+	fetch('https://test-changes--ngc-pov--joedemilner-mrm.hlx.page/data.json')
   .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
-  .then(data => document.write(data));
+  .then(data => console.log(data));
+  
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
+}
