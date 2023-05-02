a388109bce8b2087d0b6e0c6af2fd6a2dbc411a5
Joe DeMilner <joe.demilner>
2023-05-02 14:44:04 -0400

    JSON Data Block
    Created a block to read json data from google sheet

---
+++ blocks/jsondata/jsondata.js
@@ -0,0 +1,27 @@
+export default function decorate(block) {
+	  
+  	const cols = [...block.firstElementChild.children];
+  	block.classList.add(`joe-jsondata`);
+  	
+  	var iDiv = document.createElement('div');
+    iDiv.id = 'json-data';
+  	iDiv.className = 'joe-data';
+
+	const request = new Request("http://localhost:3000/data.json");
+
+	fetch(request)
+	  .then((response) => response.json())
+	  .then((data) => {
+	    for (const item of data.data) {
+			
+			var para = document.createElement("p");
+	  		para.innerHTML = '<b>key</b>: ' + item.Key + '</br> value: ' + item.Value + '</br> description: ' + item.Description + '<hr>';
+	  		iDiv.append(para);
+			console.log('ddjjd: ' + item.Key);
+		}
+	})
+	.catch(console.error);
+
+	block.appendChild(iDiv);
+
+}
